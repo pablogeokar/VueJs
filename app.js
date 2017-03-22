608c5ac90f97b36e353905a0ae707e2631a26970
Pablo George <pablogeokar@hotmail.com>
2017-03-22 18:10:13 -0300

    atualização 22/03/2017

--- app.js
+++ app.js
@@ -28,35 +28,67 @@ var menuComponent = Vue.extend({
     } 
 });
 
-var appComponent = Vue.extend({
- template: `
-  <style type="text/css">
+var billCreateComponent  = Vue.extend({
+template: `
+<form name="form" @submit.prevent="submit">
+                    <label>Vencimento:</label>
+                    <input type="text" v-model="bill.date_due"><br/><br/>
+                    <label>Nome:</label>
+                    <select v-model="bill.name">
+                        <option v-for="o in names" :value="o">{{ o }}</option>
+                    </select>
+                    <br/><br/>
+                    <label>valor:</label>
+                    <input type="text" v-model="bill.value"><br/><br/>
+                     <label>Pago?</label>
+                    <input type="checkbox" v-model="bill.done"><br/><br/>
+                    <input type="button" value="Enviar" v-on:click="submit">
+                </form>
+`,
+props: ['bill', 'formType'],
+data: function(){
+    return {
+names:[
+          "Conta de Luz",
+          "Conta de Água",
+          "Conta de telefone",
+          "Supermercado",
+          "Cartão de Crédito",
+          "Empréstimo",
+          "Gasolina"
+        ],
+    };
+},
+methods:{
+     submit: function(){
+            if(this.formType == 'INSERT'){
+                this.bills.push(this.bill);
+            }
+
+            this.bill = {
+                date_due: '',
+                name: '',
+                value: 0,
+                done: 0
+            }
+            
+            this.activedView =0;
+        },
+
+}
+});
+
+var billListComponent  = Vue.extend({
+template: `
+ <style type="text/css">
             .pago{
                 color: green;
             }
             .nao-pago{
                 color: red;
             }
-            .red{
-                color: red;
-            }
-            .green{
-                color: green;
-            }
-            .gray{
-                color: gray;
-            }
-            .minha-classe{
-                background-color: burlywood;
-            }
-        </style>
-
- <div>
-            <h1>{{ title }}</h1> 
-            <h3 :class="{ 'gray': status === false, 'green': status === 0, 'red': status >0 }">{{ status | statusGeneral }}</h3>            
-            <menu-component></menu-component>
-            <div v-if="activedView == 0">
-                <table border="1" cellpadding="10">
+ </style>
+<table border="1" cellpadding="10">
                     <thead>
                         <tr>
                             <th>#</th>
@@ -83,22 +115,61 @@ var appComponent = Vue.extend({
                         </tr>
                     </tbody>
                 </table>
+`,
+data: function(){
+    return{
+
+        bills: [
+            {date_due: "19/03/2017", name: "Conta de Luz", value: 25.99, done: 1},
+            {date_due: "21/03/2017", name: "Conta de Água", value: 31.99, done: 0},
+            {date_due: "22/03/2017", name: "Conta de telefone", value: 5.30, done: 0},
+            {date_due: "25/03/2017", name: "Supermercado", value: 125.20, done: 0},
+            {date_due: "01/04/2017", name: "Cartão de Crédito", value: 225.99, done: 0},
+            {date_due: "02/04/2017", name: "Empréstimo", value: 525.99, done: 0},
+            {date_due: "03/04/2017", name: "Gasolina", value: 170.00, done: 0},
+        ]
+    };
+},
+methods: {
+    loadBill: function(bill){
+            this.$parent.bill = bill;
+            this.$parent.activedView = 1;
+            this.$parent.formType = 'UPDATE';
+        },
+        deleteBill: function(bill){
+            if(confirm("Deseja excluir esta conta?")){
+            this.bills.$remove(bill);
+        }
+        }
+}
+});
+
+var appComponent = Vue.extend({
+ template: `
+  <style type="text/css">            
+            .red{
+                color: red;
+            }
+            .green{
+                color: green;
+            }
+            .gray{
+                color: gray;
+            }
+            .minha-classe{
+                background-color: burlywood;
+            }
+        </style>
+
+ <div>
+            <h1>{{ title }}</h1> 
+            <h3 :class="{ 'gray': status === false, 'green': status === 0, 'red': status >0 }">{{ status | statusGeneral }}</h3>            
+            <menu-component></menu-component>
+            <div v-if="activedView == 0">
+               <bill-list-component></bill-list-component> 
             </div> 
             <div v-if="activedView == 1">
-                <form name="form" @submit.prevent="submit">
-                    <label>Vencimento:</label>
-                    <input type="text" v-model="bill.date_due"><br/><br/>
-                    <label>Nome:</label>
-                    <select v-model="bill.name">
-                        <option v-for="o in names" :value="o">{{ o }}</option>
-                    </select>
-                    <br/><br/>
-                    <label>valor:</label>
-                    <input type="text" v-model="bill.value"><br/><br/>
-                     <label>Pago?</label>
-                    <input type="checkbox" v-model="bill.done"><br/><br/>
-                    <input type="button" value="Enviar" v-on:click="submit">
-                </form>
+                <bill-create-component :bill="bill" :form-type="formType"></bill-create-component>                
             </div>
         </div>
   `,
@@ -113,25 +184,7 @@ var appComponent = Vue.extend({
             name:"",
             value:0,
             done: 0
-        },
-        names:[
-          "Conta de Luz",
-          "Conta de Água",
-          "Conta de telefone",
-          "Supermercado",
-          "Cartão de Crédito",
-          "Empréstimo",
-          "Gasolina"
-        ],
-        bills: [
-            {date_due: "19/03/2017", name: "Conta de Luz", value: 25.99, done: 1},
-            {date_due: "21/03/2017", name: "Conta de Água", value: 31.99, done: 0},
-            {date_due: "22/03/2017", name: "Conta de telefone", value: 5.30, done: 0},
-            {date_due: "25/03/2017", name: "Supermercado", value: 125.20, done: 0},
-            {date_due: "01/04/2017", name: "Cartão de Crédito", value: 225.99, done: 0},
-            {date_due: "02/04/2017", name: "Empréstimo", value: 525.99, done: 0},
-            {date_due: "03/04/2017", name: "Gasolina", value: 170.00, done: 0},
-        ]
+        },        
         }
     },
     computed: {
@@ -149,30 +202,8 @@ var appComponent = Vue.extend({
         }
     },
     methods: {        
-        submit: function(){
-            if(this.formType == 'INSERT'){
-                this.bills.push(this.bill);
-            }
-
-            this.bill = {
-                date_due: '',
-                name: '',
-                value: 0,
-                done: 0
-            }
-            
-            this.activedView =0;
-        },
-        loadBill: function(bill){
-            this.bill = bill;
-            this.activedView = 1;
-            this.formType = 'UPDATE';
-        },
-        deleteBill: function(bill){
-            if(confirm("Deseja excluir esta conta?")){
-            this.bills.$remove(bill);
-        }
-        }
+               
+        
     },
     filters: {
         doneLabel: function(argument){
@@ -193,11 +224,14 @@ var appComponent = Vue.extend({
                 return "Existem " + argument +" conta(s) a ser(em) paga(s)";
             }
         }
-    } 
+    }
+ 
 });
 
 Vue.component('app-component', appComponent);
 Vue.component('menu-component', menuComponent);
+Vue.component('bill-list-component', billListComponent);
+Vue.component('bill-create-component', billCreateComponent);
 
 var app = new Vue({
     el: "#app",
