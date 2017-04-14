6cc12196eb76751c0f412cf5d1c11c912e3611ce
Pablo George <pablogeokar@hotmail.com>
2017-04-14 19:28:40 -0300

    SASS 14/04/2017

--- src/resources.js
+++
@@ -1,9 +0,0 @@
-Vue.http.options.root =  'http://192.168.1.101:8080/api';
-
-window.Bill = Vue.resource('bills{/id}',{},{
-    total: {method: 'GET', url: 'bills/total'}
-});
-
-window.BillReceive = Vue.resource('bills-receive{/id}',{},{
-    total: {method: 'GET', url: 'bills-receive/total'}
-});
